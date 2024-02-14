f26542e397ec99dc2ab52343da9456e6e2b6fd05
Clovis Junior <149280204+ClovisMallmann@users.noreply.github.com>
2024-02-14 11:20:00 -0300

    Add files via upload
    Seção 5 - Exercício simples de operações bancárias. Tal tarefa aborda conceitos dos capítulos anteriores como:
    - recapitulação de lógica de programação;
    - Classes, atributos, métodos e membros estáticos;
    - Construtores, this, sobrecarga e encapsulamento.

---
+++ Secao_5/ContaBancaria.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace Banco
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        //Cria-se Construtor para trabalhar com as variaveis privadas de uma classe
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        //Construtor com 3 argumentos
+        //uso da palavra this para reaproveitar os comandos anteriores (Numero e TItular)
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        //Operação de Depósito
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        //Operação de Saque
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia;
+            Saldo -= 5.0;
+        }
+
+
+
+        //Um método override fornece uma nova implementação de um membro herdado de uma classe base.
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular "
+                + Titular
+                + ", Saldo $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Secao_5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //Neste escopo é tudo que tem que ser executado de primeiro contato.
+            // Void porque não precisa retornar algo.
+        {
+            //Instanciando a classe fora para acesso global.
+            Banco.ContaBancaria conta;
+
+            Console.Clear();
+            Console.Write("Entre com o numero da conta: ");
+            //variavel temporaria para receber o valor da conta
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o Titular da conta: ");
+            string titular = (Console.ReadLine());
+
+            Console.Write("Haverá deposito inicial ? ");
+            //variavel temporaria para receber o valor da conta
+            char resp = char.Parse(Console.ReadLine());
+
+
+            //Verifica a resposta do cliente e atribui um valor à uma variavel qualquer.
+            if (!(resp == 's' || resp == 'S'))
+            {
+                //Chamando o Construtor de 2 argumentos caso a resposta seja N.
+                conta = new Banco.ContaBancaria(numero, titular);
+            }
+            else
+            {
+                Console.Write("Digite o valor do deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                //Chamando o contrutor com 3 argumentos caso a resposta seja S para trabalhar
+                //com a classe ContaBancaria que é restrita de acesso.
+                conta = new Banco.ContaBancaria(numero, titular, depositoInicial);
+            }
+            Console.WriteLine("\nDados da conta");
+            Console.WriteLine(conta);
+
+            //Entrada de dados para deposito, chama o objeto conta e seu metodo deposito.
+            Console.Write("\nEntre com o valor do deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta " + conta + " atualizados");
+            
+            //Entrada de dados para saque, chama o objeto conta e seu metodo Saque.
+            Console.Write("\nEntre com o valor do saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta " + conta + " atualizados");
+        }
+    }
+
+}
+    
+        
+
+        
