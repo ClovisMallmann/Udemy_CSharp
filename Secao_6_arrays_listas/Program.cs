52c10ba3441c53502209277e2d9031aa6b6c8475
Clovis Junior <149280204+ClovisMallmann@users.noreply.github.com>
2024-02-23 16:45:56 -0300

    Add files via upload
    Exercícios simples sobre listas.
    Leitura de N funcionários e seus salários, com possibilidade de acréscimo de acordo com a porcentagem determinada.

---
+++ Secao_6_arrays_listas/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program{
+
+        static void Main(string[] args)
+        {
+
+            System.Console.Clear();
+            Console.Write("Entre com o numero de funcionarios: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            int[] Funcionarios;
+            Funcionarios = new int[n];
+
+             List<string> nomes = new List<String>();
+             List<double> salarios = new List<double>();
+
+            for (int i = 0; i < n; i++)
+                  {
+                     Console.Write("Entre com nome: ");
+                     nomes.Insert(i,Console.ReadLine());
+
+                     
+                     Console.Write("Entre com salario: ");
+                     salarios.Insert(i,double.Parse(Console.ReadLine()));
+     
+                  }   
+            for (int i = 0; i < nomes.Count; i++)
+            {
+                System.Console.WriteLine(nomes[i] + " - " + salarios[i]);
+            }
+
+            Console.Write("\n Entre com a porcentagem de aumento de salario : ");
+            double porcentagem = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("\n Novos salarios !");
+
+            for (int i = 0; i < nomes.Count; i++)
+            {
+               double sal = salarios[i]*(1+porcentagem/100);
+                System.Console.WriteLine(nomes[i] + " - " + sal.ToString("c2"));
+            }
+
+        }
+    }
+}
+
+
+    
